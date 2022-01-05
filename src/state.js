0218ce74d912193e3a0c8b851977f08a06b5ccbd
joaoggnogueira <joaogabriel17.0@hotmail.com>
2022-01-05 14:36:54 -0300

    actions pattern

--- src/state.js
+++ src/state.js
@@ -6,20 +6,24 @@ export const useStateValue = () => useContext(StateContext);
 
 export const globalState = {cart: []};
 
+const actions = {
+  push(state, action) {
+    const find = state.cart.find(d => d.product.id === action.item.id);
+    if (find) {
+      find.increase();
+      return {...state};
+    }
+    return {
+      ...state,
+      cart: state.cart.concat([new OrderModel(action.item)]),
+    };
+  },
+};
+
 export const reducer = (state, action) => {
-  switch (action.type) {
-    case 'push':
-      const find = state.cart.find(d => d.product.id === action.item.id);
-      if (find) {
-        find.increase();
-        return {...state};
-      } else {
-        return {
-          ...state,
-          cart: state.cart.concat([new OrderModel(action.item)]),
-        };
-      }
-    default:
-      return state;
+  if (actions[action.type]) {
+    return actions[action.type](state, action);
+  } else {
+    return state;
   }
 };
